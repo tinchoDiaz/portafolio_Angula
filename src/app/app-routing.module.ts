d83c9d94abdbb7c77c3d20f4416e5562155497cd
Tinchodiaz <mfdiaz76.job@gmail.com>
2020-10-01 18:02:14 -0300

    Template listo con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent} from './pages/portafolio/portafolio.component';
+import { AboutComponent} from './pages/about/about.component';
+import { ItemComponent} from './pages/item/item.component';
 
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  /*Äca van todas las rutas que la aplicacion va a tener*/
+  {path: 'home', component: PortafolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMarch: 'full', redirectTo:'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
